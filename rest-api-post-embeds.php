c161a4a349994a2f4686d50064dfa5b02a7c9732
Jeremy Herve <jeremy@tagada.hu>
2017-10-20 15:32:19 +0000

    Query a different route when we need a CPT

--- rest-api-post-embeds.php
+++ rest-api-post-embeds.php
@@ -110,8 +110,8 @@ class Jeherve_Post_Embeds {
 		// Are we using the WP REST API?
 		if ( true === $atts['wpapi'] && ! absint( $blog_id ) ) {
 			// Get the post type we want to query.
-			if ( isset( $atts['post_type'] ) && 'any' != $atts['post_type'] ) {
-				$post_type = $atts['post_type'];
+			if ( isset( $args['post_type'] ) && 'any' != $args['post_type'] ) {
+				$post_type = $args['post_type'];
 			} else {
 				$post_type = 'posts';
 			}
